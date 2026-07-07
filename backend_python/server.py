722c3a9dcd5b169a5bc4b196eb32fb8f4d0ba2e0
Olufemi Davies-Davies <61275614+itsolufemi@users.noreply.github.com>
2026-07-07 01:27:28 +0100

    Register Docent vector routes

--- backend_python/server.py
+++ backend_python/server.py
@@ -9,10 +9,11 @@ from conversation_core.api.routes_llm import router as llm_router
 from conversation_core.api.routes_query import create_query_router
 
 from docent.api.routes_artworks import router as artworks_router
-from docent.api.routes_docent_retrieval import router as docent_retrieval_router
-from docent.api.routes_docent_index import router as docent_index_router
-from docent.services.docent_query_service import docent_query_engine
 from docent.api.routes_docent_embeddings import router as docent_embeddings_router
+from docent.api.routes_docent_index import router as docent_index_router
+from docent.api.routes_docent_retrieval import router as docent_retrieval_router
+from docent.api.routes_docent_vector import router as docent_vector_router
+from docent.services.docent_query_service import docent_query_engine
 
 
 app = FastAPI(
@@ -45,3 +46,4 @@ app.include_router(conversation_router)
 app.include_router(docent_retrieval_router)
 app.include_router(docent_index_router)
 app.include_router(docent_embeddings_router)
+app.include_router(docent_vector_router)
